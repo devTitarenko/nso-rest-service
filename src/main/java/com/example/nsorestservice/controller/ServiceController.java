7217098dd660098d9e7e77015cdd8e18d17dfd93
vtytarenko <vtytarenko@lohika.com>
2019-08-23 13:16:07 +0300

    delete service

--- src/main/java/com/example/nsorestservice/controller/ServiceController.java
+++ src/main/java/com/example/nsorestservice/controller/ServiceController.java
@@ -4,13 +4,9 @@ import com.example.nsorestservice.model.NsoService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.*;
 
 @RestController
 public class ServiceController extends ParentController {
@@ -33,5 +29,14 @@ public class ServiceController extends ParentController {
             set(CONTENT_TYPE, NsoMediaType.JSON_DATA.getStringValue());
         }};
         return callNso(url, POST, nsoService, headers, Object.class);
+    }
+
+    @DeleteMapping("deleteService/{id}")
+    public ResponseEntity deleteService(@PathVariable("id") String serviceId) {
+        String url = getNsoApiUrl() + API + RUNNING + SERVICES + "service/" + serviceId;
+        HttpHeaders headers = new HttpHeaders() {{
+            set(ACCEPT, NsoMediaType.JSON_DATA.getStringValue());
+        }};
+        return callNso(url, DELETE, headers, Object.class);
     }
 }
