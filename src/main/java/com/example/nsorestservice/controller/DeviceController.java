bd8ae5ce1db5121f881f41c34f67f3306a48e45c
vtytarenko <vtytarenko@lohika.com>
2019-08-23 15:24:35 +0300

    delete device

--- src/main/java/com/example/nsorestservice/controller/DeviceController.java
+++ src/main/java/com/example/nsorestservice/controller/DeviceController.java
@@ -4,20 +4,19 @@ import com.example.nsorestservice.model.NsoDevice;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpMethod.PUT;
+import static org.springframework.http.HttpMethod.*;
 
 @RestController
 public class DeviceController extends ParentController {
 
+    private static final String DEVICES = "devices/";
+    private static final String DEVICE = "device/";
+
     @GetMapping(value = "getDevices", produces = MediaType.APPLICATION_JSON_VALUE)
     public Object getDevices() {
-        String url = getNsoApiUrl() + API + RUNNING + "devices/";
+        String url = getNsoApiUrl() + API + RUNNING + DEVICES;
         HttpHeaders headers = new HttpHeaders() {{
             set(ACCEPT, NsoMediaType.JSON_DATA.getStringValue());
         }};
@@ -26,10 +25,19 @@ public class DeviceController extends ParentController {
 
     @PutMapping("addDevice")
     public ResponseEntity addDevice(@RequestBody NsoDevice nsoDevice) {
-        String url = getNsoApiUrl() + API + RUNNING + "devices/device/" + nsoDevice.getDevice().getName();
+        String url = getNsoApiUrl() + API + RUNNING + DEVICES + DEVICE + nsoDevice.getDevice().getName();
         HttpHeaders headers = new HttpHeaders() {{
             set(CONTENT_TYPE, NsoMediaType.JSON_DATA.getStringValue());
         }};
         return callNso(url, PUT, nsoDevice, headers, Object.class);
+    }
+
+    @DeleteMapping("deleteDevice/{id}")
+    public ResponseEntity deleteDevice(@PathVariable("id") String deviceId) {
+        String url = getNsoApiUrl() + API + RUNNING + DEVICES + DEVICE + deviceId;
+        HttpHeaders headers = new HttpHeaders() {{
+            set(ACCEPT, NsoMediaType.JSON_DATA.getStringValue());
+        }};
+        return callNso(url, DELETE, headers, Object.class);
     }
 }
